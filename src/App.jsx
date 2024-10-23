96e3527cc0ede4124d395936850e15457be2da87
anik-das <anikdas1085@gmail.com>
2024-10-23 17:50:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Blogs></Blogs>
+    </div>
+  );
+};
+
+export default App;
